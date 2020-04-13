294ebf0c18206258e145e161cb8429792a0330cd
sindre <sindreblakseth@gmail.com>
2020-04-13 21:16:59 +0200

    Changed the track.

--- main.py
+++ main.py
@@ -7,19 +7,13 @@ from ai.controller import train
 
 def main():
     rails = [
-        # st.Straight("std"),
-        # st.Straight("std"),
-        st.Curve(2, 45, direction=st.Curve.Left),
-        st.Straight("std"),
-        st.Straight("std"),
+        st.Straight('half'),
         st.Curve(2, 45, direction=st.Curve.Left),
         st.Curve(2, 45, direction=st.Curve.Left),
         st.Curve(2, 45, direction=st.Curve.Left),
-        # st.Straight("std"),
-        # st.Straight("std"),
         st.Curve(2, 45, direction=st.Curve.Left),
-        st.Straight("std"),
-        st.Straight("std"),
+        st.Straight('half'),
+        st.Curve(2, 45, direction=st.Curve.Left),
         st.Curve(2, 45, direction=st.Curve.Left),
         st.Curve(2, 45, direction=st.Curve.Left),
         st.Curve(2, 45, direction=st.Curve.Left),
